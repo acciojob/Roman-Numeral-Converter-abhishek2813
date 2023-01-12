f1d158c68ee51e8a50d4e8a5dbd858ea62059a34
Abhishek Pandey <98042094+abhishek2813@users.noreply.github.com>
2023-01-12 22:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ function convertToRoman(num) {
     };
 
   //your code here
+	  let result = '';
+  
+  for (key in map) {
+    result += key.repeat(Math.floor(num / map[key]));
+    num %= map[key];
+  }
+  
+  return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
